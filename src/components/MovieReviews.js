1e531d99edbeb2ddcebc881e8a2837406c4bfd44
Max Goodman <max.goodman@flatironschool.com>
2018-07-02 14:34:08 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React, { Component } from 'react';
+
+const MovieReviews =(props) => {
+	return(
+		<ul className="review-list">
+			<li className="review"></li>
+			<li className="review"></li>
+			<li className="review"></li>
+		</ul>
+	)
+}
+
+MovieReviews.defaultProps = {
+	reviews: []
+}
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,22 @@
 // Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import MovieReviews from './MovieReviews';
+
+class SearchableMovieReviewsContainer extends React.Component{
+ constructor(props){
+ 	super(props)
+ 	this.state = {
+ 		reviews: [],
+ 		searchTerm: ""
+ 	}
+ }
+
+ render(){
+ 	return(
+ 		<div className="searchable-movie-reviews">
+ 		<MovieReviews />
+ 		</div>
+ 		)
+ }
+}
+export default SearchableMovieReviewsContainer;
